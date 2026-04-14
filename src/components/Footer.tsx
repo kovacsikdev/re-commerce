d19b9e657ceeaf8d6080af204721a3a12d8747f5
Michael Kovacsik <kovacsikdev@gmail.com>
2026-04-13 19:09:54 -0700

    add github link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,43 @@
-import "./Footer.css"
+import "./Footer.css";
 
 export const Footer = () => {
   return (
     <footer className="footer">
       <div>
         <h2>UMBRELLA CORPORATION</h2>
-      <div>© 1968 UMBRELLA CORPORATION. ALL RIGHT RESERVED. UNHAZARD COMPLIANT</div>
+        <div>
+          © 1968 UMBRELLA CORPORATION. ALL RIGHT RESERVED. UNHAZARD COMPLIANT
+        </div>
       </div>
       <div>
         <h2>RE Commerce prototype built by Michael Kovacsik</h2>
-        <p className="color-primary-light"><a href="https://kovacsikdev.github.io/" target="_blank" rel="noopener noreferrer">kovacsikdev.github.io</a></p>
-        <p className="color-primary-light"><a href="mailto:kovacsikdev@gmail.com" target="_blank" rel="noopener noreferrer">kovacsikdev@gmail.com</a></p>
+        <p className="color-primary-light">
+          <a
+            href="https://kovacsikdev.github.io/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            kovacsikdev.github.io
+          </a>
+        </p>
+        <p className="color-primary-light">
+          <a
+            href="mailto:kovacsikdev@gmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            kovacsikdev@gmail.com
+          </a>
+        </p>
+        <p className="color-primary-light">
+          <a
+            href="https://github.com/kovacsikdev/re-commerce"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Github
+          </a>
+        </p>
       </div>
     </footer>
   );
